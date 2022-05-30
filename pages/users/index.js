460be172be020989b15c0ee58839822797cb0125
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-31 02:39:01 +0600

    dynamic route setup

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 
 const index = ({users}) => {
     return (
         <div>
             <h2>This is User main page!!! {users.length}</h2>
+            {
+                users.map(user => <div key={user.id}>
+                        <h4>Name: {user.name}
+                            <Link href={`/users/${user.id}`}>  
+                                   <button>Explore</button>
+                            </Link>
+                        </h4>
+                    </div>)
+            }
         </div>
     );
 };
---
+++ pages/users/managers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const managers = () => {
+    return (
+        <div>
+            <h2>This is All About Manages!!!</h2>
+        </div>
+    );
+};
+
+export default managers;
